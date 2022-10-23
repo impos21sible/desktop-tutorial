d2884d64c0bfc9ce8d04e32468b36b65bea20de7
impos21sible <116497492+impos21sible@users.noreply.github.com>
2022-10-24 00:59:02 +0300

    Add files via upload

---
+++ tutorial/обучение/ДЗ.1/ConsoleApp1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Math;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Введите A=");
+            double A = Convert.ToInt32(Console.ReadLine());
+            
+            Console.Write("Введите B=");
+            double B = Convert.ToInt32(Console.ReadLine());
+            
+            Console.Write("Введите C=");
+            double C = Convert.ToInt32(Console.ReadLine());
+            double D = Pow(B, 2.0) - 4 * A * C;
+            
+            Console.WriteLine($"D={D}");
+
+            double x1 = (-B + Sqrt(D))/(2.0*A);
+            double x2 = (-B - Sqrt(D))/(2.0*A);
+
+            if (x2<x1) 
+            {
+                Console.WriteLine($"x2={x2}");
+                Console.WriteLine($"x1={x1}");
+            }
+
+            else
+            {
+                Console.WriteLine($"x1={x1}");
+                Console.WriteLine($"x2={x2}");
+            }
+            
+            
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ tutorial/обучение/ДЗ.2/ConsoleApp2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double A1,A2,B1,B2,C1,C2;
+            Console.Write("Введите A = ");
+             A1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите B1 = ");
+             B1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите C1 = ");
+             C1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите A2 = ");
+            A2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите B2 = ");
+             B2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите C2 = ");
+             C2 = Convert.ToInt32(Console.ReadLine());
+
+            double D = (1*A1*B2-A2*B1);
+            Console.WriteLine($"D={D}");
+
+            double x = (C1*B2 - C2*B1)/D;
+            Console.WriteLine($"x={x}");
+
+            double y = (A1*C2 - A2*C1)/D;
+
+            Console.WriteLine($"y={y}");
+
+            double РешениеС1 = (A1*x+B1*y);
+            double РешениеС2 = (A2*x+B2*y);
+            Console.WriteLine($"Провека числа С1 ={РешениеС1}");
+            Console.WriteLine($"Провека числа С2 ={РешениеС2}");
+
+        }
+    }
+}
